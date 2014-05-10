f8737f105baa76bc4020eab2d90ab1588ea943c7
Gerardo Ortega <geraldavid7@gmail.com>
2014-05-10 12:18:57 -0600

    Run Forrest, run

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nicabarometro2::Application.config.secret_key_base = '65458be288328295152b4204add0832135d1d3bb61e4ec297b3ac097fc4a2f4e16c88ef8fb5c77cb77a4dfc0bdee3fc052c38b9d2fa85c781b451b361872bffb'
